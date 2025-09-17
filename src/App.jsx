2ccb0ea71e62b2acf8e7a48c96be30a664943d3c
kirsenyaj <kirsenyajvillanueva@gmail.com>
2025-09-17 22:07:57 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+export default function App() {
+  return (
+    <div className="app">
+      {}
+      <div className="top">
+        <div className="box peach"></div>
+
+        <div className="middle">
+          <div className="inner-middle">
+            <div className="half red"></div>
+            <div className="half pink"></div>
+          </div>
+        </div>
+
+        <div className="box peach"></div>
+      </div>
+
+      {}
+      <div className="middle-section">
+        <div className="big pink"></div>
+        <div className="column">
+          <div className="col pink"></div>
+          <div className="col pink"></div>
+        </div>
+      </div>
+
+      {}
+      <div className="bottom">
+        <div className="side purple"></div>
+        <div className="mini-wrapper">
+          <div className="mini peach"></div>
+          <div className="mini peach"></div>
+        </div>
+        <div className="side purple"></div>
+      </div>
+    </div>
+  );
+}
