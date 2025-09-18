526d46fa0410344c1422ca1ddc67f94bfa125ce0
kirsenyaj <kirsenyajvillanueva@gmail.com>
2025-09-18 21:44:12 +0800

    Added functions and props to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 export default function App() {
+  function TopContent(props) {
+    return <div className="half red" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '45px', color: 'white' }}>{props.text}</div>;
+  }
+  
+  function BottomContent(props) {
+    return <div className="mini peach" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '45px', color: 'white' }}>{props.text}</div>;
+  }
+
   return (
     <div className="app">
-      {}
       <div className="top">
         <div className="box peach"></div>
-
         <div className="middle">
           <div className="inner-middle">
-            <div className="half red"></div>
-            <div className="half pink"></div>
+            <TopContent text="Kirsen Yaj" />
+            <TopContent text="Villanueva" />
           </div>
         </div>
-
         <div className="box peach"></div>
       </div>
-
-      {}
+      
       <div className="middle-section">
         <div className="big pink"></div>
         <div className="column">
@@ -23,13 +27,12 @@ export default function App() {
           <div className="col pink"></div>
         </div>
       </div>
-
-      {}
+      
       <div className="bottom">
         <div className="side purple"></div>
         <div className="mini-wrapper">
-          <div className="mini peach"></div>
-          <div className="mini peach"></div>
+          <BottomContent text="C-PCIT9" />
+          <BottomContent text="IT3A" />
         </div>
         <div className="side purple"></div>
       </div>
